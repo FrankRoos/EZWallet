caae8ab6f274819ee7cd67b70d9b781aa6f1004f
ANGELO IANNIELLI s317887 <s317887@studenti.polito.it>
2023-05-25 14:26:31 +0200

    Import missing function

--- code/controllers/auth.js
+++ code/controllers/auth.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 import { User } from '../models/User.js';
 import jwt from 'jsonwebtoken';
-import { verifyAuth } from './utils.js';
+import { verifyAuth, handleString } from './utils.js';
 
 /**
  *  Register a new user in the system
