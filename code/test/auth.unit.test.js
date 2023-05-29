fedd01f942efb442f36580f67babe6ffd2bda693
ANGELO IANNIELLI s317887 <s317887@studenti.polito.it>
2023-05-30 00:24:00 +0200

    Test Login

--- code/test/auth.unit.test.js
+++ code/test/auth.unit.test.js
@@ -2,6 +2,8 @@ import request from 'supertest';
 import { app } from '../app';
 import { User } from '../models/User.js';
 import jwt from 'jsonwebtoken';
+import * as auth from '../controllers/auth.js';
+import * as utils from '../controllers/utils.js';
 const bcrypt = require("bcryptjs")
 
 jest.mock("bcryptjs")
@@ -20,8 +22,39 @@ describe("registerAdmin", () => {
 })
 
 describe('login', () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    test('Simple login', async () => {
+        const existingUser = {
+            email: 'test@example.com',
+            password: 'hashedPassword',
+            _id: 'someId',
+            username: 'testUser',
+            save: jest.fn().mockResolvedValue({ refreshToken: 'refreshToken' }), 
+          };
+        const mockReq = {
+            body: {
+                email: "angelo.iannielli99@gmail.com",
+                password: "12345678"
+            },
+            /*cookie: {
+                accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuZ2Vsby5pYW5uaWVsbGk5OUBnbWFpbC5jb20iLCJpZCI6IjY0NjI2MjliNWYzZWU0NzVjNGI3NjJhMyIsInVzZXJuYW1lIjoiYW5nZWxvIiwicm9sZSI6IlJlZ3VsYXIiLCJpYXQiOjE2ODUxOTg2MzAsImV4cCI6MTY4NTE5ODYzMH0.tCqmMl60NWG43bmi3aqZ4zNEPOuPZ_lyZG7g9CKxQV8",
+                refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuZ2Vsby5pYW5uaWVsbGk5OUBnbWFpbC5jb20iLCJpZCI6IjY0NjI2MjliNWYzZWU0NzVjNGI3NjJhMyIsInVzZXJuYW1lIjoiYW5nZWxvIiwicm9sZSI6IlJlZ3VsYXIiLCJpYXQiOjE2ODUxOTg2MzAsImV4cCI6MTY4NTgwMzQzMH0.8KRWV60rOsVSM8haLIL3eplyZTelaxt5KQNkvUzv10Q"
+            }*/
+        }
+        const mockRes = {
+            cookie: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+
+        /*const verify = jest.fn(()=> {return true});
+        utils.verifyAuth = verify;*/
+
+        jest.spyOn(jwt, "sign").mockImplementation(() => {return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuZ2Vsby5pYW5uaWVsbGk5OUBnbWFpbC5jb20iLCJpZCI6IjY0NjI2MjliNWYzZWU0NzVjNGI3NjJhMyIsInVzZXJuYW1lIjoiYW5nZWxvIiwicm9sZSI6IlJlZ3VsYXIiLCJpYXQiOjE2ODUxOTg2MzAsImV4cCI6MTY4NTE5ODYzMH0.tCqmMl60NWG43bmi3aqZ4zNEPOuPZ_lyZG7g9CKxQV8"})
+        jest.spyOn(bcrypt, "compare").mockImplementation(() => { return true })
+        jest.spyOn(User, "findOne").mockImplementation(() => { return existingUser })
+        // jest.spyOn(existingUser, "save").mockImplementation(() => {  })
+        await auth.login(mockReq, mockRes)
+        expect(mockRes.status).toHaveBeenCalledWith(200);
     });
 });
 
@@ -30,3 +63,4 @@ describe('logout', () => {
         expect(true).toBe(true);
     });
 });
+
