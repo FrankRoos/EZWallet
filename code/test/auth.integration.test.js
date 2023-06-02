2717d39fdbc4a46a9ffa5de23ee49ae59c2c48fd
Filippo Restori <resto84773@gmail.com>
2023-06-02 15:28:39 +0200

    added register integration test

--- code/test/auth.integration.test.js
+++ code/test/auth.integration.test.js
@@ -25,7 +25,30 @@ afterAll(async () => {
 });
 
 describe('register', () => {
-});
+  beforeEach(async()=>{
+    await User.deleteMany();
+  })
+    test('Should return status 200 OK on successful registration', async () => {
+      const requestBody = {
+        username: 'test',
+        email: 'test@example.com',
+        password: 'password123'
+      };
+  
+      const response = await request(app)
+        .post('/api/register')
+        .send(requestBody);
+  
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual({ data: { message: 'User added succesfully' } });
+      const user = await User.findOne({ email: requestBody.email });
+      expect(user).toBeDefined();
+      expect(user.username).toBe(requestBody.username);
+      expect(user.email).toBe(requestBody.email);
+      
+    });
+  })
+
 
 describe("registerAdmin", () => { 
     test('Dummy test, change it', () => {
